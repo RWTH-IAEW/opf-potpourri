d1774cbb4533e297a6d4f4562a9005885c9c1465
Bengisu Yilmaz <bengisu.yilmaz@rwth-aachen.de>
2023-05-19 15:27:46 +0200

    Initial_commit

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ else:
     print("System is not running on Windows Subsystem for Linux (WSL).")
 
 potpourri.dcopf(tc=test_case, solver="glpk", print_output=True, print_solver_output=True, print_model=False)
-potpourri.dcopf(tc=test_case, solver="gurobi", print_output=True, print_solver_output=True, print_model=False, objective="quadratic")
+# potpourri.dcopf(tc=test_case, solver="gurobi", print_output=True, print_solver_output=True, print_model=False, objective="quadratic")
 potpourri.acopf(tc=test_case, solver="ipopt", print_output=True, print_solver_output=True, print_model=False)
 potpourri.acopf(tc=test_case, solver="ipopt", print_output=True, print_solver_output=True, print_model=False, objective="min_gen")
 potpourri.acopf(tc=test_case, solver="ipopt", print_output=True, print_solver_output=True, print_model=False, objective="max_cost")
-potpourri.scopf(tc=test_case, solver="gurobi", print_output=True, print_solver_output=True, print_model=False)
+# potpourri.scopf(tc=test_case, solver="gurobi", print_output=True, print_solver_output=True, print_model=False)
