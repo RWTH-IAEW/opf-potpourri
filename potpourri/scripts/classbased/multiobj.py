edb6bdef3d36bacc0002753bd96b6ca8ad7e724b
f.lohse <franca.lohse@rwth-aachen.de>
2024-02-14 15:17:17 +0100

    find optimal point (losses, wind)

--- potpourri/scripts/classbased/multiobj.py
+++ potpourri/scripts/classbased/multiobj.py
@@ -1,15 +1,16 @@
 import copy
-
+import pickle
 import pandas as pd
-import simbench as sb
 import numpy as np
 import pyomo.environ as pe
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 from potpourri.models.class_based.HC_ACOPF import HC_ACOPF
 from potpourri.scripts.classbased.plot_functions import plot_wind_hc_results
 
 
-def max_wind_min_loss(net, **kwargs):
+def max_wind_min_loss(net, n=11, **kwargs):
     hc = HC_ACOPF(net, SWmax=10000, SWmin=0, peGmax=1000000)
     hc.solve()
     hc.add_OPF()
@@ -25,7 +26,7 @@ def max_wind_min_loss(net, **kwargs):
     p_wind = []
     p_loss = []
 
-    for i in range(11):
+    for i in range(n):
         hc.model.eps = 1 - i / 10
         hc.solve(solver='mindtpy', mip_solver='gurobi')
 
@@ -39,12 +40,12 @@ def max_wind_min_loss(net, **kwargs):
             p_loss.append(None)
 
     fig, ax = plt.subplots()
-    ax.scatter(p_loss, p_wind)
+    ax.plot(p_loss, p_wind, 'o-.')
 
     return obj, p_wind, p_loss
 
 
-def weighted_sum(net, **kwargs):
+def weighted_sum(net, n=11, **kwargs):
     hc = HC_ACOPF(net, SWmax=10000, SWmin=0, peGmax=1000000)
     hc.solve()
     hc.add_OPF()
@@ -80,7 +81,7 @@ def weighted_sum(net, **kwargs):
     p_loss = []
     hcs = []
 
-    for i in range(21):
+    for i in range(n):
         hc.model.w_wind = i / 20 / p_wind_max
         hc.model.w_loss = (1 - i / 20) / p_loss_min
 
@@ -100,7 +101,7 @@ def weighted_sum(net, **kwargs):
     return obj, p_wind, p_loss
 
 
-def epsilon_constraint(net, n=10, plot_hc=False, **kwargs):
+def pareto_front(net, n=10, **kwargs):
     hc = HC_ACOPF(net, SWmax=10000, SWmin=0, peGmax=1000000)
     hc.solve()
     hc.add_OPF()
@@ -140,27 +141,17 @@ def epsilon_constraint(net, n=10, plot_hc=False, **kwargs):
         expr=sum(hc.model.pLfrom[l] + hc.model.pLto[l] for l in hc.model.L) <= hc.model.eps)
 
     # generate pareto front
-    obj, p_wind, p_loss, nets = [], [], [], []
+    p_wind, p_loss, nets = [], [], []
 
     step = (p_loss_max - p_loss_min) / n
-    steps = np.arange(p_loss_min, p_loss_max, step)
+    steps = np.linspace(p_loss_max, p_loss_min, n)
 
-    while step > 0.1:
-        obj_eps, p_wind_eps, p_loss_eps, nets_eps = epsilon_constraint2(hc, step, steps)
+    p_wind, p_loss, nets, p_wind_opt, p_loss_opt = epsilon_constraint(hc, step, steps)
 
-        obj += obj_eps
-        p_wind += p_wind_eps
-        p_loss += p_loss_eps
-        nets += nets_eps
+    # p_wind = np.append(p_wind, p_wind_max)
+    # p_loss = np.append(p_loss, p_loss_max)
 
-        step = (p_loss_eps[-2] - p_loss_eps[-3]) / n
-        steps = np.arange(p_loss_eps[-3], p_loss_eps[-2], step)
-
-    p_wind.append(p_wind_max)
-    p_loss.append(p_loss_max)
-    obj.append(obj_max)
-
-    results = pd.DataFrame({'obj': obj, 'p_wind': p_wind, 'p_loss': p_loss})
+    results = pd.DataFrame({'p_wind': p_wind, 'p_loss': p_loss})
 
     # # plot pareto front
     # fig, ax = plt.subplots()
@@ -168,49 +159,189 @@ def epsilon_constraint(net, n=10, plot_hc=False, **kwargs):
     # ax.set_xlabel('$P_{losses}$ [MW]')
     # ax.set_ylabel('$P_{wind}$ [MW]')
 
-    return obj, p_wind, p_loss, nets, results
+    return p_wind, p_loss, nets, results
 
 
-def epsilon_constraint2(hc, step, steps):
-    # step = (p_max - p_min) / n
-    # steps = np.arange(p_min, p_max, step)
+def p_wind_loss_opt(net, n=10, **kwargs):
+    hc = HC_ACOPF(net, SWmax=10000, SWmin=0, peGmax=1000000)
+    hc.solve()
+    hc.add_OPF()
 
-    obj = []
-    p_wind = []
-    p_loss = []
+    if kwargs.get('tap_linear', False):
+        hc.add_tap_changer_linear()
+
+    # if kwargs.get('tap_discrete', False):
+    #     hc.add_tap_changer_discrete()
+
+    if kwargs.get('SWmin', False):
+        hc.change_vals('SWmin', kwargs.get('SWmin'))
+
+    hc.solve(solver='mindtpy', mip_solver='gurobi')
+
+    p_wind_max = pe.value(hc.model.OBJ)
+    p_loss_max = pe.value(sum(hc.model.pLfrom[l] + hc.model.pLto[l] for l in hc.model.L))
+
+    hc.model.OBJ.deactivate()
+
+    # get minimum losses
+    def obj_min_loss(model):
+        return sum(model.pLfrom[l] + model.pLto[l] for l in model.L)
+    hc.model.OBJ_loss = pe.Objective(rule=obj_min_loss, sense=pe.minimize)
+
+    hc.solve(solver='mindtpy', mip_solver='gurobi')
+    p_loss_min = pe.value(hc.model.OBJ_loss)
+    p_wind_min = pe.value(sum(hc.model.pG[w] for w in hc.model.WIND))
+
+    hc.model.OBJ_loss.deactivate()
+    hc.model.OBJ.activate()
+
+    if kwargs.get('tap_discrete', False):
+        hc.add_tap_changer_discrete()
+
+    # add loss objective function as constraint
+    hc.model.eps = pe.Param(within=pe.Reals, initialize=p_loss_min, mutable=True)
+    hc.model.constr_loss = pe.Constraint(
+        expr=sum(hc.model.pLfrom[l] + hc.model.pLto[l] for l in hc.model.L) <= hc.model.eps)
+
+    # generate pareto front
+    p_wind, p_loss, nets = [[p_wind_min]], [[p_loss_min]], [copy.deepcopy(hc.net)]
+    p_wind_opt = p_wind_min
+    p_loss_opt = p_loss_min
+
+    p_wind_next = p_wind_max
+    p_loss_next = p_loss_max
+
+    step = (p_loss_max - p_loss_min) / (n-1)
+    steps = np.linspace(p_loss_min+step, p_loss_max-step, (n-2))
+
+    # prev_p_wind_opt = p_wind_min
+    # prev_p_loss_opt = p_loss_min
+    # tolerance = 1e-5  # Define your tolerance here
+    # same_value_counter = 0
+
+    while step > 0.005:
+        tqdm.write(str(steps))
+
+        p_w = [p_wind_opt, p_wind_next]
+        p_l = [p_loss_opt, p_loss_next]
+
+        p_wind_eps, p_loss_eps, nets_eps, p_wind_opt, p_loss_opt, p_wind_next, p_loss_next = epsilon_constraint(hc, steps, p_w, p_l, mode='opt')
+
+        nets.append(nets_eps)
+        p_wind.append(p_wind_eps)
+        p_loss.append(p_loss_eps)
+
+        if p_wind_opt is None:
+            p_wind_opt = p_wind_eps[-1]
+            p_loss_opt = p_loss_eps[-1]
+
+        # # If the new values are the same as the previous ones within the tolerance, increment the counter
+        # if np.isclose(p_wind_opt, prev_p_wind_opt, atol=tolerance) and np.isclose(p_loss_opt, prev_p_loss_opt, atol=tolerance):
+        #     same_value_counter += 1
+        #     # If the counter reaches 3, break the loop
+        #     if same_value_counter == 5:
+        #         break
+        # else:
+        #     same_value_counter = 0  # Reset counter if values are different
+        #
+        # # Update the previous values
+        # prev_p_wind_opt = p_wind_opt
+        # prev_p_loss_opt = p_loss_opt
+
+        # # Get the next smaller value from p_loss_eps
+        # smaller_elements = p_loss_eps[p_loss_eps < p_loss_opt]
+        # next_smaller_value = np.max(smaller_elements) if smaller_elements.size > 0 else p_loss_opt
+
+        # Calculate the new steps
+        step = abs(p_loss_opt - p_loss_next) / (n-1)
+        steps = np.linspace(p_loss_opt - step, p_loss_next + step, n - 2)
+
+    p_wind.append(p_wind_max)
+    p_loss.append(p_loss_max)
+
+    results = pd.DataFrame({'p_wind': p_wind, 'p_loss': p_loss})
+
+    # # plot pareto front
+    # fig, ax = plt.subplots()
+    # ax.plot(p_loss, p_wind, 'o-.')
+    # ax.set_xlabel('$P_{losses}$ [MW]')
+    # ax.set_ylabel('$P_{wind}$ [MW]')
+
+    return p_wind, p_loss, nets, results, p_wind_opt, p_loss_opt
+
+
+def epsilon_constraint(hc, steps, p_w, p_l, mode=None):
+    p_wind = [p_w[0]]
+    p_loss = [p_l[0]]
     nets = []
 
-    for i in steps:
+    p_wind_opt = None
+    p_loss_opt = None
+
+    for i in tqdm(steps):
         hc.model.eps = i
         hc.solve(solver='mindtpy', mip_solver='gurobi')
 
-        if pe.check_optimal_termination(hc.results) | (
-                hc.results.solver.termination_condition == pe.TerminationCondition.feasible):
-            obj.append(pe.value(hc.model.OBJ))
-            p_wind.append(pe.value(sum(hc.model.pG[w] for w in hc.model.WIND)))
-            p_loss.append(pe.value(sum(hc.model.pLfrom[l] + hc.model.pLto[l] for l in hc.model.L)))
-            nets.append(copy.deepcopy(hc.net))
+        p_wind.append(pe.value(sum(hc.model.pG[w] for w in hc.model.WIND)))
+        p_loss.append(pe.value(sum(hc.model.pLfrom[l] + hc.model.pLto[l] for l in hc.model.L)))
+        nets.append(copy.deepcopy(hc.net))
 
-            # calculate difference between last two values
-            if len(p_wind) > 1:
-                p_wind_diff = p_wind[-1] - p_wind[-2]
-                if abs(p_wind_diff - step) < 1e-3:
-                    break
+        if not mode:
+            continue
+        # calculate difference between last two values
+        if len(p_wind) > 1:
+            p_wind_diff = p_wind[-1] - p_wind[-2]
+            p_loss_diff = p_loss[-1] - p_loss[-2]
+            if p_wind_diff - p_loss_diff <= -1e-3:
+                p_wind_opt = p_wind[-2]
+                p_loss_opt = p_loss[-2]
 
-        else:
-            hc.model.OBJ.display()
-            obj.append(None)
-            p_wind.append(None)
-            p_loss.append(None)
-            nets.append(None)
+                if p_wind_diff <= 0:
+                    p_wind_next = p_wind[-1]
+                    p_loss_next = p_loss[-1]
+                else:
+                    p_wind_next = p_wind[-3]
+                    p_loss_next = p_loss[-3]
 
-    return obj, p_wind, p_loss, nets
+                p_wind.pop(0)
+                p_loss.pop(0)
+
+                return np.array(p_wind), np.array(p_loss), nets, p_wind_opt, p_loss_opt, p_wind_next, p_loss_next
+
+    if mode:
+        p_wind_next = None
+        p_loss_next = None
+
+        if len(p_wind) > 1:
+            p_wind_diff = p_w[-1] - p_wind[-1]
+            p_loss_diff = p_l[-1] - p_loss[-1]
+
+            if p_wind_diff - p_loss_diff <= -1e-3:
+                p_wind_opt = p_wind[-1]
+                p_loss_opt = p_loss[-1]
+
+                if p_wind_diff <= 0:
+                    p_wind_next = p_w[-1]
+                    p_loss_next = p_l[-1]
+                else:
+                    p_wind_next = p_wind[-2]
+                    p_loss_next = p_loss[-2]
+
+        p_wind.pop(0)
+        p_loss.pop(0)
+
+        return np.array(p_wind), np.array(p_loss), nets, p_wind_opt, p_loss_opt, p_wind_next, p_loss_next
+
+    return np.array(p_wind), np.array(p_loss), nets, p_wind_opt, p_loss_opt
 
 
 if __name__ == '__main__':
-    net = sb.get_simbench_net("1-HV-mixed--0-no_sw")
+    with open('C:/Users/f.lohse/PycharmProjects/potpourri/potpourri/data/simbench_hv_grid_with_potential_pkl.pkl', 'rb') as f:
+        net = pickle.load(f)
 
-    case = 'hL'
+    # net = sb.get_simbench_net("1-HV-mixed--0-no_sw")
+
+    case = 'lW'
 
     factors = net.loadcases.loc[case]
     net.load.p_mw *= factors['pload']
@@ -219,17 +350,3 @@ if __name__ == '__main__':
     net.sgen.scaling[net.sgen.type == 'PV'] = factors['PV_p']
     net.sgen.scaling[(net.sgen.type != 'Wind') & (net.sgen.type != 'Solar')] = factors['RES_p']
     net.ext_grid.vm_pu = factors['Slack_vm']
-
-    # obj_eps, p_wind_eps, p_loss_eps, hcs_eps = epsilon_constraint(net)
-    # obj_ws, p_wind_ws, p_loss_ws = weighted_sum(net)
-    # #
-    # fig, ax = plt.subplots()
-    # ax.scatter(p_loss_eps, p_wind_eps, label='Epsilon Constraint')
-    # ax.scatter(p_loss_ws, p_wind_ws, label='Weighted Sum')
-    # ax.legend()
-    # ax.set_xlabel('$P_{losses}$ [MW]')
-    # ax.set_ylabel('$P_{wind}$ [MW]')
-
-    # p_loss = p_loss_ws + p_loss_eps
-    # p_wind = p_wind_ws + p_wind_eps
-    # ax.plot(p_loss, p_wind, 'o-.')
--- potpourri/scripts/classbased/run_scenarios.py
+++ potpourri/scripts/classbased/run_scenarios.py
@@ -1,35 +1,320 @@
+import copy
+import datetime
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandapower.timeseries import OutputWriter
+
 from potpourri.models.class_based.HC_ACOPF import HC_ACOPF
+from potpourri.scripts.classbased.init_pyo_from_pp_res import init_pyo_from_dcpp
 
 import pandapower as pp
 import pickle
+import pyomo.environ as pe
+import numpy as np
+from tqdm import tqdm
+import random
+
+from potpourri.models.class_based.pyo_to_net import pyo_sol_to_net_res
+from potpourri.scripts.classbased.plot_functions import plot_wind_hc_results
+
 
 def add_scenario_load_sgen_to_net(net, busses):
     net.load.in_service = False
     net.sgen.in_service = False
 
-    pp.create_loads(net, busses, p_mw=0, name='load_scenario')
-    pp.create_sgens(net, busses, p_mw=0, name='sgen_scenario')
+    load_ind = pp.create_loads(net, busses, p_mw=0, name='load_scenario')
+    sgen_ind = pp.create_sgens(net, busses, p_mw=0, name='sgen_scenario')
+
+    return load_ind, sgen_ind
+
+
+def create_output_writer(net, time_steps, output_dir):
+    ow = OutputWriter(net, time_steps, output_path=output_dir, output_file_type=".xlsx", log_variables=list())
+
+    ow.log_variable('res_bus', 'vm_pu')
+    ow.log_variable('res_sgen', 'q_mvar')
+    ow.log_variable('res_line', 'loading_percent')
+    ow.log_variable('res_line', 'pl_mw')
+    ow.log_variable('res_line', 'ql_mvar')
+    ow.log_variable('res_trafo', 'loading_percent')
+    ow.log_variable('res_ext_grid', 'p_mw')
+    ow.log_variable('res_ext_grid', 'q_mvar')
+
+    wind_index = net.sgen.index
+    ow.log_variable('res_sgen', 'p_mw')
+    ow.log_variable('res_sgen', ['p_mw', 'y_wind'], eval_function=get_p_wind(ind=len(wind_index)), eval_name='wind_p')
+    ow.log_variable('res_sgen', 'q_mvar')
+
+    return ow
+
+
+def get_p_wind(ind):
+    def eval_func(results, n_columns=ind):
+        p_wind = results[:, 0] * results[:, 1]
+        return p_wind
+
+    return eval_func
+
+
+def run_scenarios(net, scenarios, output_dir, SWmin=10, peGmax=10000, cases=None):
+    busses = scenarios['load_sgen_busses']
+    if cases:
+        pass
+    else:
+        cases = range(len(scenarios['load']))
+    n_scenarios = len(scenarios['load'][0][0])
+
+    load_ind, sgen_ind = add_scenario_load_sgen_to_net(net, busses)
+
+    obj = {}
+
+    for case in cases:
+        obj[case] = []
+
+        hc = HC_ACOPF(net, SWmin=SWmin, peGmax=peGmax)
+
+
+        sc = 0
+        for i, d in enumerate(load_ind):
+            hc.model.pD[d] = scenarios['load'][case][i, sc]
+            hc.model.qD[d] = scenarios['load_q'][case][i, sc]
+
+        for i, g in enumerate(sgen_ind):
+            hc.model.pG[g] = scenarios['sgen'][case][i, sc]
+            hc.model.qG[g] = scenarios['sgen_q'][case][i, sc]
+
+        if any(hc.net.trafo.shift_degree):
+            init_pyo_from_dcpp(hc.net, hc.model)
+
+        hc.solve()
+        hc.add_OPF()
+        # hc.add_loss_obj()
+        # hc.model.eps = 0.9
+
+
+        # w = hc.model.WIND.data()
+        # rand_w = random.sample(w, round(len(w)/4))
+        # print(rand_w)
+        # for w in rand_w:
+        #     # hc.model.SWmin[w] = 20
+        #     # hc.model.SWmax[w] = 150
+        #     hc.model.y[w].fix(0.)
+
+        output_path = output_dir + '_' + str(case)
+        ow = create_output_writer(hc.net, range(n_scenarios), output_path)
+        ow.init_all(hc.net)
+
+        hc.solve(solver='mindtpy', mip_solver='gurobi')
+
+        # use solver.status == 'ok' instead?
+        pf_converged = hc.results.solver.termination_condition == (
+                pe.TerminationCondition.optimal or pe.TerminationCondition.feasible)
+
+        ow.time_step = sc
+        ow.save_results(hc.net, sc, pf_converged=pf_converged, ctrl_converged=pf_converged)
+
+        obj[case].append(pe.value(hc.model.OBJ))
+
+        for sc in tqdm(range(1, n_scenarios)):
+            for i, d in enumerate(load_ind):
+                hc.model.pD[d] = scenarios['load'][case][i, sc]
+                hc.model.qD[d] = scenarios['load_q'][case][i, sc]
+
+            for i, g in enumerate(sgen_ind):
+                hc.model.pG[g] = scenarios['sgen'][case][i, sc]
+                hc.model.qG[g] = scenarios['sgen_q'][case][i, sc]
+
+            hc.solve(solver='mindtpy', mip_solver='gurobi')
+
+            # use solver.status == 'ok' instead?
+            pf_converged = hc.results.solver.termination_condition == (
+                    pe.TerminationCondition.optimal or pe.TerminationCondition.feasible)
+
+            ow.time_step = sc
+            ow.save_results(hc.net, sc, pf_converged=pf_converged,
+                            ctrl_converged=pe.check_optimal_termination(hc.results))
+
+            obj[case].append(pe.value(hc.model.OBJ))
+
+    return obj
+
+
+def scenario_supply(scenarios):
+    n_scenarios = len(scenarios['load'][0][0])
+    n_cases = len(scenarios['load'])
+
+    scenarios['supply'] = [ [] for _ in range(n_cases) ]  # Initialize a list for each case
+    scenarios['supply_sum'] = [ [] for _ in range(n_cases) ]  # Initialize a list for each case
+
+    for case in range(n_cases):
+        p_supply_case = []  # Initialize a list to store p_supply values for the current case
+        for sc in range(n_scenarios):
+            p_load = scenarios['load'][case][:, sc]
+            p_sgen = scenarios['sgen'][case][:, sc]
+
+            p_supply = p_sgen - p_load
+            p_supply_case.append(p_supply)  # Append p_supply to the list for the current case
+
+        scenarios['supply'][case] = np.array(p_supply_case)
+        scenarios['supply_sum'][case] = np.sum(p_supply_case, axis=1)  # Sum over columns and store in 'supply_sum'
+
+def plot_scenario_net(net, results_dir, n_cases):
+    for case in range(n_cases):
+        p_mw_file = os.path.join(results_dir, 'case_' + str(case), 'res_sgen', 'p_mw.xlsx')
+        p_mw = pd.read_excel(p_mw_file, index_col=0)
+        q_mvar_file = os.path.join(results_dir, 'case_' + str(case), 'res_sgen', 'q_mvar.xlsx')
+        q_mvar = pd.read_excel(q_mvar_file, index_col=0)
+
+        vm_pu_file = os.path.join(results_dir, 'case_' + str(case), 'res_bus', 'vm_pu.xlsx')
+        vm_pu = pd.read_excel(vm_pu_file, index_col=0)
+
+        ext_grid_p_mw_file = os.path.join(results_dir, 'case_' + str(case), 'res_ext_grid', 'p_mw.xlsx')
+        ext_grid_p_mw = pd.read_excel(ext_grid_p_mw_file, index_col=0)
+        ext_grid_q_mvar_file = os.path.join(results_dir, 'case_' + str(case), 'res_ext_grid', 'q_mvar.xlsx')
+        ext_grid_q_mvar = pd.read_excel(ext_grid_q_mvar_file, index_col=0)
+
+        line_loading_percent_file = os.path.join(results_dir, 'case_' + str(case), 'res_line', 'loading_percent.xlsx')
+        line_loading_percent = pd.read_excel(line_loading_percent_file, index_col=0)
+        line_pl_mw_file = os.path.join(results_dir, 'case_' + str(case), 'res_line', 'pl_mw.xlsx')
+        line_pl_mw = pd.read_excel(line_pl_mw_file, index_col=0)
+        line_ql_mvar_file = os.path.join(results_dir, 'case_' + str(case), 'res_line', 'ql_mvar.xlsx')
+        line_ql_mvar = pd.read_excel(line_ql_mvar_file, index_col=0)
+
+        trafo_loading_percent_file = os.path.join(results_dir, 'case_' + str(case), 'res_trafo', 'loading_percent.xlsx')
+        trafo_loading_percent = pd.read_excel(trafo_loading_percent_file, index_col=0)
+
+        for sc in range(len(p_mw)):
+            net.res_sgen.p_mw = p_mw.loc[sc]
+            net.res_sgen.q_mvar = q_mvar.loc[sc]
+
+            net.res_bus.vm_pu = vm_pu.loc[sc]
+
+            net.res_ext_grid.p_mw = ext_grid_p_mw.loc[sc]
+            net.res_ext_grid.q_mvar = ext_grid_q_mvar.loc[sc]
+
+            net.res_line.loading_percent = line_loading_percent.loc[sc]
+            net.res_line.pl_mw = line_pl_mw.loc[sc]
+            net.res_line.ql_mvar = line_ql_mvar.loc[sc]
+
+            net.res_trafo.loading_percent = trafo_loading_percent.loc[sc]
+
+            try:
+                plot_wind_hc_results([net])
+            except:
+                print('Error plotting results of case ' + str(case) + ' scenario ' + str(sc))
+
+
+
+def box_plot_scenarios(results_dir, n_cases, cases=None):
+    p_wind = []
+
+    # for case in range(n_cases):
+    #     p_mw_file = os.path.join(results_dir, 'case_' + str(case), 'res_sgen', 'p_mw.xlsx')
+    #     p_mw = pd.read_excel(p_mw_file, index_col=0)
+    #     p_w = p_mw['wind_p']
+    #     p_wind.append(p_w)
+
+    for case in cases:
+        p_mw_y_file = os.path.join(results_dir, 'case_' + str(case), 'res_sgen', '[\'p_mw\', \'y_wind\'].xlsx')
+        p_mw_y = pd.read_excel(p_mw_y_file, index_col=0)
+        p_w = p_mw_y.sum(axis=1)
+        p_wind.append(p_w)
+
+
+    fig, ax = plt.subplots()
+    ax.boxplot(p_wind[0:3])
+
+    ax.set_title("Windintegrationspotenzial")
+    ax.set_ylabel('$P_{Wind}$ [MW]')
+    ax.set_xlabel('Netznutzungsfall')
+    ax.set_xticks([1, 2, 3], ['hL', 'hW', 'hPV'])
+
+    fig, ax = plt.subplots()
+    ax.boxplot(p_wind[3:5])
+
+    ax.set_title("Windintegrationspotenzial")
+    ax.set_ylabel('$P_{Wind}$ [MW]')
+    ax.set_xlabel('Netznutzungsfall')
+    ax.set_xticks([1, 2], ['lW', 'lPV'])
+
+    return p_wind
+
+
+def std_dev_scenarios(dir):
+    with open(dir, "rb") as f:
+        scenarios = pickle.load(f)
+
+    scenarios['std_dev_load'] = {}
+    scenarios['std_dev_sgen'] = {}
+    scenarios['std_dev_supply'] = {}
+
+    for case in range(len(scenarios['load'])):
+        std_dev_load = np.std(scenarios['load'][case], axis=1)
+        std_dev_sgen = np.std(scenarios['sgen'][case], axis=1)
+        std_dev_supply = np.std(scenarios['supply'][case], axis=0)
+        scenarios['std_dev_load'][case] = std_dev_load
+        scenarios['std_dev_sgen'][case] = std_dev_sgen
+        scenarios['std_dev_supply'][case] = std_dev_supply
+
+    fig, ax = plt.subplots()
+    ax.bar(range(len(scenarios['std_dev_sgen'][3])), scenarios['std_dev_sgen'][3])
+    ax.set_xlabel("Knoten")
+    ax.set_ylabel("Standardabweichung [MW]")
+
+    return scenarios
+
+
+def std_dev_p_wind_scenarios(dir, case):
+    p_mw_file = os.path.join(results_dir, 'case_' + str(case), 'res_sgen', 'p_mw.xlsx')
+    p_mw = pd.read_excel(p_mw_file, index_col=0)
+
+
+def create_weighted_p_wind(probabilities, p_wind):
+    min = np.min(probabilities)
+    weights = probabilities / min * 3
+    weights = weights.round()
+
+    p_wind_weighted = []
+    for p_w in p_wind:
+        p_w_weighted = np.array([])
+        for i, w in enumerate(weights):
+            p_w_weighted = np.concatenate((p_w_weighted, np.repeat(p_w[i], w.astype(int))))
+
+        p_wind_weighted.append(p_w_weighted)
+
+    fig, ax = plt.subplots()
+    ax.boxplot(p_wind_weighted)
 
 
 if __name__ == '__main__':
-    with open("../../data/scenarios/1-HV-mixed--0-no_sw_scenario_types.pkl", "rb") as f:
+    # with open("../../data/scenarios/1-HV-mixed--0-no_sw_scenario_types.pkl", "rb") as f:
+    #     net = pickle.load(f)
+    #
+    # with open("../../data/scenarios/1-HV-mixed_loadcases_assumption_scenarios_with_q.pkl", "rb") as f:
+    #     scenarios = pickle.load(f)
+    #
+    # results_dir = '../../results/scenarios/1-HV-mixed_loadcases_distribution_scenarios/'
+
+    grid = '1-HV-mixed--0-no_sw'
+    # grid = 'hv_grid'
+    type = "assumption"
+
+    with open('C:\\Users\\f.lohse\PycharmProjects\potpourri\potpourri\data\simbench_hv_grid_with_potential_pkl.pkl',
+              'rb') as f:
+        net = pickle.load(f)
+        net.bus.windpot_p_mw = net.bus.windpot_p_mw.where(net.bus.windpot_p_mw <= 200, 200)
+
+    with open("../../data/scenarios/" + grid + "_scenario_types.pkl", "rb") as f:
         net = pickle.load(f)
 
-    with open("../../data/scenarios/1-HV-mixed_loadcases_assumption_scenarios_with_q.pkl", "rb") as f:
+    with open("../../data/scenarios/" + grid + "_loadcases_" + type + "_scenarios_with_q.pkl", "rb") as f:
         scenarios = pickle.load(f)
 
-    add_scenario_load_sgen_to_net(net, scenarios['load_sgen_busses'])
+    results_dir = '../../results/scenarios/' + grid + '_loadcases_' + type + '_scenarios/'
 
-    net.load.p_mw[net.load.name == 'load_scenario'] = scenarios['load'][0][:, 0]
-    net.load.q_mvar[net.load.name == 'load_scenario'] = scenarios['load_q'][0][:, 0]
-
-    net.sgen.p_mw[net.sgen.name == 'sgen_scenario'] = scenarios['sgen'][0][:, 0]
-    net.sgen.q_mvar[net.sgen.name == 'sgen_scenario'] = scenarios['sgen_q'][0][:, 0]
-
-    hc = HC_ACOPF(net)
-    hc.solve()
-    hc.add_OPF()
-
+    obj = run_scenarios(net, scenarios, results_dir + 'case', peGmax=10000, cases=[3], SWmin=10)
 
     # for i in range(len(scenarios['load'][0][0])):
     #     net.load.p_mw[net.load.name == 'load_scenario'] = scenarios['load'][0][:, i]
@@ -37,4 +322,3 @@ if __name__ == '__main__':
     #
     #     net.sgen.p_mw[net.sgen.name == 'sgen_scenario'] = scenarios['sgen'][0][:, i]
     #     net.sgen.q_mvar[net.sgen.name == 'sgen_scenario'] = scenarios['sgen_q'][0][:, i]
-
